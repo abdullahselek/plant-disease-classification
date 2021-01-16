d6777ee33cd32095f9378d551d4730de71ca398b
Abdullah Selek <abdullahselek@gmail.com>
2021-01-16 21:32:33 +0000

    Extend classifier

--- plant_disease_classification/plant_disease_classifier.py
+++ plant_disease_classification/plant_disease_classifier.py
@@ -5,6 +5,8 @@ import torchvision.transforms as transforms
 
 from torch.autograd import Variable
 from PIL import Image
+from io import BytesIO
+from typing import Optional
 from plant_disease_classification.network import CNN, constant
 
 
@@ -44,18 +46,36 @@ class PlantDiseaseClassifier(object):
         tensor = tensor.to(DEVICE)
         return tensor
 
+    def __load_image_from_bytes(self, image_data: bytes):
+        image = Image.open(BytesIO(image_data))
+        tensor = self.tranform(image).float()
+        tensor = Variable(tensor, requires_grad=True)
+        tensor = tensor.to(DEVICE)
+        return tensor
+
     def __batch_data(self, tensor):
         return tensor[None, ...]
 
-    def classify(self, image_path: str):
+    def classify(
+        self, image_path: Optional[str] = None, image_data: Optional[bytes] = None
+    ):
         """Returns a prediction class.
         Args:
-          image_path (str):
+          image_path (Optional[str]):
             Image path that is going to be used in classification.
+          image_data (Optional[bytes]):
+            Image data that is going to be used in classification.
         Returns:
           Prediction class (str)"""
 
-        tensor = self.__load_image(image_path)
+        tensor = None
+        if image_path:
+            tensor = self.__load_image(image_path=image_path)
+        elif image_data:
+            tensor = self.__load_image_from_bytes(image_data=image_data)
+        if tensor is None:
+            raise Exception("Please provide image path or data of your image!")
+
         output = self.model(self.__batch_data(tensor))
         predicted = torch.argmax(output)
         classes = constant.classes
